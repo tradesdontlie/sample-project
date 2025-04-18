2d58a04c50c86add0c3e9bc971339eb262ba1157
tradesdontlie <babcockstj@gmail.com>
2025-04-18 13:07:06 -0400

    Add sample web files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('demo-button');
+  
+  button.addEventListener('click', () => {
+    alert('Hello! This button was clicked!');
+  });
+});
